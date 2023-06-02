3b350e9a2d0f78ceaf1a34276346e2db4864fde1
Saman Rana <saman.ranaaa@gmail.com>
2023-06-02 13:42:19 -0500

    Add User logour backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -48,6 +48,16 @@ router.post(
   );
 
 
+  // Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+
 
 
 
