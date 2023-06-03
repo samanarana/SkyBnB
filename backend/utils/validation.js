adda3f8f9ff559f7bb8af353838cf0f25fc47175
Saman Rana <saman.ranaaa@gmail.com>
2023-06-02 21:11:41 -0500

    validation error function

---
+++ backend/utils/validation.js
@@ -0,0 +1,27 @@
+const { validationResults } = require('express-validator');
+
+//middleware for formatting errors from express validator middleware
+
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = {};
+        validationErrors
+        .array()
+        .forEach(error => errors[error.param] = error.msg);
+
+        const err = Error("Bad Request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad Request."
+        next(err);
+    }
+
+    next();
+}
+
+
+module.exports = {
+    handleValidationErrors
+};
