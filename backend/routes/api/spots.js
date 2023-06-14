29e4f90bee0dd2f18a783825ea28d053559ce1c6
Saman Rana <saman.ranaaa@gmail.com>
2023-06-14 18:20:36 -0500

    spots routes done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -56,7 +56,7 @@ router.post('/', requireAuth, async (req, res) => {
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
     const { user } = req;
-    console.log(user, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++THIS SHOULD BE MY USER")
+    //console.log(user, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++THIS SHOULD BE MY USER")
 
 
     if (!address || !city || !state || !country || !lat || !lng || !name || !description || !price) {
@@ -96,6 +96,127 @@ router.post('/', requireAuth, async (req, res) => {
 
     res.json(newSpot);
 });
+
+
+// ROUTE TO ADD AN IMAGE TO A SPOT BASED ON THE SPOT ID
+router.post('/:SpotId/images', requireAuth, async (req, res) => {
+    const { url, preview } = req.body;
+    const SpotId = req.params.SpotId;
+    const userId = req.user.id;
+
+    if (!url || typeof preview !== 'boolean') {
+        return res.status(400).json({
+            message: "Bad Request",
+            errors: {
+                "url": "Url is required",
+                "preview": "Preview should be true or false"
+            }
+        });
+    }
+
+    const spot = await Spot.findOne({
+        where: {
+            id: SpotId,
+            owner_id: userId,
+        }
+    });
+
+    if (!spot) {
+        return res.status(404).json({ message: "Spot couldn't be found" });
+    }
+
+    const newImage = await SpotImage.create({ spot_id: SpotId, url, preview });
+
+    res.json(newImage);
+});
+
+
+
+// ROUTE TO UPDATE AN EXISTING SPOT
+router.put('/:spotId', requireAuth, async (req, res) => {
+    const spotId = req.params.spotId;
+    const userId = req.user.id;
+
+    const {
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    } = req.body;
+
+    if (!address || !city || !state || !country || !lat || !lng || !name || !description || !price) {
+        return res.status(400).json({
+            message: "Bad Request",
+            errors: {
+                "address": "Street address is required",
+                "city": "City is required",
+                "state": "State is required",
+                "country": "Country is required",
+                "lat": "Latitude is not valid",
+                "lng": "Longitude is not valid",
+                "name": "Name must be less than 50 characters",
+                "description": "Description is required",
+                "price": "Price per day is required"
+            }
+        });
+    }
+
+    const spot = await Spot.findOne({
+        where: {
+            id: spotId,
+            owner_id: userId
+        }
+    });
+
+    if (!spot) {
+        return res.status(404).json({ message: "Spot couldn't be found" });
+    }
+
+    const updatedSpot = await spot.update({
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    });
+
+    res.json(updatedSpot);
+});
+
+
+// ROUTE TO DELETE A SPOT
+router.delete('/:spotId', requireAuth, async (req, res, next) => {
+    const spotId = req.params.spotId;
+
+        // Find the spot
+        const spot = await Spot.findOne({
+            where: {
+                id: spotId,
+                owner_id: req.user.id // Check ownership
+            }
+        });
+
+        // If spot not found or does not belong to the user, throw an error
+        if (!spot) {
+            return res.status(404).json({ message: "Spot couldn't be found" });
+        }
+
+        // Delete the spot
+        await spot.destroy();
+        return res.status(200).json({ message: "Successfully deleted" });
+
+});
+
+
 
 
 module.exports = router;
