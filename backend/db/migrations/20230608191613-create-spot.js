8d07c61303d7f0a231c5a00f1d26392c20ef3950
Saman Rana <saman.ranaaa@gmail.com>
2023-06-17 18:14:20 -0500

    still cant figure out why the users table doesnt exist

--- backend/db/migrations/20230608191613-create-spot.js
+++ backend/db/migrations/20230608191613-create-spot.js
@@ -13,7 +13,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: "User",
+          model: "Users",
           field: "id"
         }
       },
