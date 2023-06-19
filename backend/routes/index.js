52fde27e5dadc2a8e3e067b8db9905304d4a971f
Saman Rana <saman.ranaaa@gmail.com>
2023-06-19 16:57:29 -0500

    edit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,26 +3,6 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-//const routes = require('./routes');
-const userRoutes = require('./users');
-const spotRoutes = require('./spots');
-const spotImagesRouter = require('./spotimages');
-const reviewsRouter = require('./reviews');
-const bookingsRouter = require('./bookings');
-const reviewImagesRouter = require('./reviewimages');
-
-
-
-// Register your API routes
-router.use('/users', userRoutes);
-router.use('/spots', spotRoutes);
-router.use('/spotimages', spotImagesRouter);
-router.use('/reviews', reviewsRouter);
-router.use('/bookings', bookingsRouter);
-router.use('/reviewimages', reviewImagesRouter);
-//app.use(routes);
-
-
 
 // backend/routes/index.js
 // ...
