a80dc13a15129687b0d6ba2eca00d5fe89942a8d
Saman Rana <saman.ranaaa@gmail.com>
2023-06-19 02:03:31 -0500

    took out ids from seeders

--- backend/db/seeders/20230608201607-demo-spot-image.js
+++ backend/db/seeders/20230608201607-demo-spot-image.js
@@ -11,25 +11,21 @@ module.exports = {
     options.tableName = "SpotImages";
     return queryInterface.bulkInsert(options, [
     {
-      id: 1,
       spot_id: 1,
       url: 'http://placekitten.com/200/300',
       preview: true
     },
     {
-      id: 2,
       spot_id: 1,
       url: 'https://www.google.com/',
       preview: false
     },
     {
-      id: 3,
       spot_id: 2,
       url: 'http://placekitten.com/200/300',
       preview: true
     },
     {
-      id: 4,
       spot_id: 3,
       url: 'https://www.twitter.com/',
       preview: true
