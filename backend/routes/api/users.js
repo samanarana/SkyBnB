cd1ced8b695c227dc421350eff103fa60e080e69
Saman Rana <saman.ranaaa@gmail.com>
2023-06-18 20:34:11 -0500

    had 2 signup routes, got rid of one

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,29 +33,29 @@ const validateSignup = [
 
 
 // Sign up
-router.post(
-  '',
-  validateSignup,
-  async (req, res) => {
-    const { firstName, lastName, email, password, username } = req.body;
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ firstName, lastName, email, username, hashedPassword });
+// router.post(
+//   '',
+//   validateSignup,
+//   async (req, res) => {
+//     const { firstName, lastName, email, password, username } = req.body;
+//     const hashedPassword = bcrypt.hashSync(password);
+//     const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
-    const safeUser = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      username: user.username,
-    };
+//     const safeUser = {
+//       id: user.id,
+//       firstName: user.firstName,
+//       lastName: user.lastName,
+//       email: user.email,
+//       username: user.username,
+//     };
 
-    await setTokenCookie(res, safeUser);
+//     await setTokenCookie(res, safeUser);
 
-    return res.json({
-      user: safeUser
-    });
-  }
-);
+//     return res.json({
+//       user: safeUser
+//     });
+//   }
+// );
 
 
 
