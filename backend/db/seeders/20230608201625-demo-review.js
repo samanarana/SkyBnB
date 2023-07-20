deab06e4dc982253234dd49be8a7b324556733cf
Saman Rana <saman.ranaaa@gmail.com>
2023-07-19 19:25:03 -0500

    review seed fix

--- backend/db/seeders/20230608201625-demo-review.js
+++ backend/db/seeders/20230608201625-demo-review.js
@@ -12,21 +12,25 @@ module.exports = {
     options.tableName = "Reviews";
     return queryInterface.bulkInsert(options, [
       {
+        userId: 1,
         spotId: 1,
         review: 'This spot is amazing!',
         stars: 5,
       },
       {
+        userId: 2,
         spotId: 1,
         review: 'Great experience at this spot.',
         stars: 4,
       },
       {
+        userId: 1,
         spotId: 2,
         review: 'The spot was okay, nothing special.',
         stars: 3,
       },
       {
+        userId: 3,
         spotId: 3,
         review: 'I highly recommend this spot!',
         stars: 5,
