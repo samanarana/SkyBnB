16c2fb0cb3580536d46760cd1489afe2d959eed3
Saman Rana <saman.ranaaa@gmail.com>
2023-08-18 07:55:10 -0500

    Signup page is working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,25 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    //console.log("Response Data:", data);
+    dispatch(setUser(data));
+    //console.log("Dispatching User:", data.user);
+    return response;
+  };
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
